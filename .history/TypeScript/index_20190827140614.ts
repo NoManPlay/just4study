57b5353b47d6ed90de0b14c31f0160caf8a1be67
NoManPlay <819134128@qq.com>
2019-08-27 14:08:15 +0800

    add ts

---
+++ .history/TypeScript/index_20190827140614.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: qtx
+ * @Date: 2019-08-27 13:57:10
+ * @LastEditors: qtx
+ * @LastEditTime: 2019-08-27 14:06:14
+ * @Description: 
+ */
+console.log('hello world');
---
+++ TypeScript/index.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: qtx
+ * @Date: 2019-08-27 13:57:10
+ * @LastEditors: qtx
+ * @LastEditTime: 2019-08-27 14:08:02
+ * @Description: 
+ */
+console.log('hello world');
